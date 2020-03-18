64196b6fbd028554a9722e8d2939f9d68cd099cc
PRIYANJAN <PRIYANJAN@PRIYANJAN>
2020-03-18 22:31:56 +0530

    first commit : web service for doctor appointment app

---
+++ docappoint-restful-webservice/src/main/java/com/docappoint/rest/webservices/DocappointRestfulWebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.docappoint.rest.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocappointRestfulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocappointRestfulWebserviceApplication.class, args);
+	}
+}
---
+++ docappoint-restful-webservice/src/test/java/com/docappoint/rest/webservices/docappointrestfulwebservice/DocappointRestfulWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docappoint.rest.webservices.docappointrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocappointRestfulWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
